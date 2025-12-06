0c378ae17015f294dbc84254242e69b3912367e9
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-12-06 18:40:25 -0300

    Implementando modelo.

--- backend/src/main/java/com/okay_bank/backend/infra/entity/Address.java
+++ backend/src/main/java/com/okay_bank/backend/infra/entity/Address.java
@@ -1,5 +1,7 @@
 package com.okay_bank.backend.infra.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class Address {
+	@Column(name = "address_zip_code", nullable = false, length = 15)
 	private String zipCode;
+	@Column(name = "address_name", nullable = false, length = 30)
 	private String name;
+	@Column(name = "address_distric", nullable = false, length = 30)
 	private String district;
+	@Column(name = "address_city", nullable = false, length = 30)
 	private String city;
+	@Column(name = "address_state", nullable = false, length = 30)
 	private String state;
 }
--- backend/src/main/java/com/okay_bank/backend/infra/entity/Customer.java
+++ backend/src/main/java/com/okay_bank/backend/infra/entity/Customer.java
@@ -2,9 +2,13 @@ package com.okay_bank.backend.infra.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,13 +22,20 @@ import lombok.NoArgsConstructor;
 public class Customer {
 	@Id
 	private String id;
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false, unique = true, length = 100)
 	private String name;
+	@Column(nullable = false, unique = true, length = 50)
 	private String email;
+	@Column(nullable = false, unique = true, length = 30)
 	private String phone;
+	@Column(nullable = false, unique = true, length = 8)
 	private String password;
+	@Column(nullable = false)
 	private CustomerType customerType;
+	@Column(nullable = false, unique = true, length = 14)
 	private String document;
+	@Embedded
 	private Address address;
+	@OneToMany(mappedBy = "customer", fetch = FetchType.LAZY,  cascade = CascadeType.MERGE)
 	private List<Account> accounts;
 }
