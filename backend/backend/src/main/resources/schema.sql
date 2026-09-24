e27324418a1ec4592f2cac7b7d03f712a1dfed53
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2026-09-24 18:42:41 -0300

    feat: Implement SQL schema.

---
+++ backend/backend/src/main/resources/schema.sql
@@ -0,0 +1,127 @@
+CREATE TABLE agency_tb (
+    id VARCHAR PRIMARY KEY,
+    number VARCHAR(10) NOT NULL UNIQUE,
+    creation_date DATE NOT NULL
+);
+
+CREATE TABLE customer_tb (
+    id VARCHAR PRIMARY KEY,
+    document VARCHAR(20) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    phone VARCHAR(30) NOT NULL UNIQUE,
+    customer_type VARCHAR(2) NOT NULL,
+
+    -- Address
+    postal_code VARCHAR(10) NOT NULL,
+    address_name VARCHAR(100) NOT NULL,
+    address_number INTEGER NOT NULL,
+    district VARCHAR(30) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    state VARCHAR(30) NOT NULL,
+
+    CHECK (customer_type IN ('PF', 'PJ'))
+);
+
+CREATE TABLE employee_tb (
+    id VARCHAR PRIMARY KEY,
+    matriculation VARCHAR(10) NOT NULL UNIQUE,
+    document VARCHAR(20) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    salary NUMERIC(30,2) NOT NULL,
+
+    -- Address
+    postal_code VARCHAR(10) NOT NULL,
+    address_name VARCHAR(100) NOT NULL,
+    address_number INTEGER NOT NULL,
+    district VARCHAR(30) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    state VARCHAR(30) NOT NULL,
+
+    email VARCHAR(30) NOT NULL UNIQUE,
+    phone VARCHAR(30) NOT NULL UNIQUE,
+    journey INTEGER NOT NULL,
+    employee_type VARCHAR(20) NOT NULL,
+
+    agency_id VARCHAR NOT NULL,
+
+    FOREIGN KEY (agency_id) REFERENCES agency_tb(id),
+
+    CHECK (employee_type IN ('BANKING', 'MANAGER'))
+);
+
+CREATE TABLE account_tb (
+    id VARCHAR PRIMARY KEY,
+    number VARCHAR(20) NOT NULL UNIQUE,
+    balance NUMERIC(30,2) NOT NULL DEFAULT 0.00,
+    creation_date DATE NOT NULL,
+    password VARCHAR NOT NULL UNIQUE,
+    account_type VARCHAR(20) NOT NULL,
+
+    customer_id VARCHAR NOT NULL,
+    agency_id VARCHAR NOT NULL,
+
+    FOREIGN KEY (customer_id) REFERENCES customer_tb(id),
+
+    FOREIGN KEY (agency_id) REFERENCES agency_tb(id),
+
+    CHECK (account_type IN ('SAVINGS', 'CHECKING'))
+);
+
+CREATE TABLE card_tb (
+    id VARCHAR PRIMARY KEY,
+    number VARCHAR(16) NOT NULL UNIQUE,
+    holder VARCHAR(100) NOT NULL,
+    validation_date DATE NOT NULL,
+    flag VARCHAR(20) NOT NULL,
+    cvv VARCHAR(3) NOT NULL UNIQUE,
+    password VARCHAR NOT NULL UNIQUE,
+    card_type VARCHAR(20) NOT NULL,
+    "limit" NUMERIC(30,2) NOT NULL DEFAULT 0.00,
+
+    agency VARCHAR(20) NOT NULL,
+    account VARCHAR(20) NOT NULL,
+
+    customer_id VARCHAR NOT NULL,
+
+    FOREIGN KEY (customer_id) REFERENCES customer_tb(id),
+
+    CHECK (card_type IN ('CREDIT', 'DEBIT'))
+);
+
+CREATE TABLE transfer_tb (
+    id VARCHAR PRIMARY KEY,
+
+    -- Payer
+    payer_name VARCHAR(100) NOT NULL,
+    payer_document VARCHAR(20) NOT NULL,
+    payer_institution VARCHAR(50) NOT NULL,
+    payer_account_number VARCHAR(10) NOT NULL,
+    payer_agency_number VARCHAR(15) NOT NULL,
+
+    -- Receiver
+    receiver_name VARCHAR(100) NOT NULL,
+    receiver_document VARCHAR(20) NOT NULL,
+    receiver_institution VARCHAR(50) NOT NULL,
+    receiver_account_number VARCHAR(20) NOT NULL,
+    receiver_agency_number VARCHAR(20) NOT NULL,
+
+    "value" NUMERIC(30,2) NOT NULL,
+    transfer_date_and_time TIMESTAMP NOT NULL,
+    transfer_type VARCHAR(20) NOT NULL,
+
+    CHECK (transfer_type IN ('PIX', 'TED')),
+
+    CHECK ("value" > 0)
+);
+
+CREATE TABLE account_transfer_tb (
+    id VARCHAR PRIMARY KEY,
+
+    transfer_id VARCHAR NOT NULL UNIQUE,
+    account_id VARCHAR NOT NULL UNIQUE,
+
+    FOREIGN KEY (transfer_id) REFERENCES transfer_tb(id),
+
+    FOREIGN KEY (account_id) REFERENCES account_tb(id)
+);
